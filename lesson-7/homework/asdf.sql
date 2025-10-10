a987358bf38f68d5626b9ce95e6a6db934e9142e
Diyorbek4913 <naziraliyevdiyorbek11@gmail.com>
2025-10-10 17:13:38 +0500

    Create asdf.sql

---
+++ lesson-7/homework/asdf.sql
@@ -0,0 +1,118 @@
+1. SELECT MIN(Price) AS MinPrice
+FROM Products;
+2. SELECT MAX(Salary) AS MaxSalary
+FROM Employees;
+3. SELECT COUNT(*) AS TotalCustomers
+FROM Customers;
+4. SELECT COUNT(DISTINCT Category) AS UniqueCategories
+FROM Products;
+5. SELECT SUM(SaleAmount) AS TotalSales_Product7
+FROM Sales
+WHERE ProductID = 7;
+6. SELECT AVG(Age) AS AverageEmployeeAge
+FROM Employees;
+7. SELECT DepartmentName, COUNT(*) AS EmployeeCount
+FROM Employees
+GROUP BY DepartmentName;
+8. SELECT Category, 
+       MIN(Price) AS MinPrice, 
+       MAX(Price) AS MaxPrice
+FROM Products
+GROUP BY Category;
+9. SELECT CustomerID, SUM(SaleAmount) AS TotalSales
+FROM Sales
+GROUP BY CustomerID;
+10. SELECT DepartmentName, COUNT(*) AS EmployeeCount
+FROM Employees
+GROUP BY DepartmentName
+HAVING COUNT(*) > 5;
+11. SELECT 
+    p.Category,
+    SUM(s.SaleAmount) AS TotalSales,
+    AVG(s.SaleAmount) AS AverageSales
+FROM Sales s
+JOIN Products p ON s.ProductID = p.ProductID
+GROUP BY p.Category;
+12. SELECT COUNT(*) AS HREmployeeCount
+FROM Employees
+WHERE DepartmentName = 'HR';
+13. SELECT 
+    DepartmentName,
+    MAX(Salary) AS HighestSalary,
+    MIN(Salary) AS LowestSalary
+FROM Employees
+GROUP BY DepartmentName;
+14. SELECT 
+    DepartmentName,
+    AVG(Salary) AS AverageSalary
+FROM Employees
+GROUP BY DepartmentName;
+15. SELECT 
+    DepartmentName,
+    AVG(Salary) AS AverageSalary,
+    COUNT(*) AS EmployeeCount
+FROM Employees
+GROUP BY DepartmentName;
+16. SELECT 
+    Category,
+    AVG(Price) AS AvgCategoryPrice
+FROM Products
+GROUP BY Category
+HAVING AVG(Price) > 400;
+17. SELECT 
+    YEAR(SaleDate) AS SaleYear,
+    SUM(SaleAmount) AS TotalSales
+FROM Sales
+GROUP BY YEAR(SaleDate)
+ORDER BY SaleYear;
+18. SELECT 
+    CustomerID,
+    COUNT(*) AS OrderCount
+FROM Orders
+GROUP BY CustomerID
+HAVING COUNT(*) >= 3;
+19.SELECT 
+    DepartmentName,
+    AVG(Salary) AS AvgSalary
+FROM Employees
+GROUP BY DepartmentName
+HAVING AVG(Salary) > 60000;
+20. SELECT 
+    Category,
+    AVG(Price) AS AvgPrice
+FROM Products
+GROUP BY Category
+HAVING AVG(Price) > 150;
+21. SELECT 
+    CustomerID,
+    SUM(SaleAmount) AS TotalSales
+FROM Sales
+GROUP BY CustomerID
+HAVING SUM(SaleAmount) > 1500;
+22. SELECT 
+    DepartmentName,
+    SUM(Salary) AS TotalSalary,
+    AVG(Salary) AS AvgSalary
+FROM Employees
+GROUP BY DepartmentName
+HAVING AVG(Salary) > 65000;
+23. SELECT 
+    o.CustomerID,
+    SUM(CASE WHEN o.Freight > 50 THEN o.Freight ELSE 0 END) AS TotalOver50,
+    MIN(o.Freight) AS LeastPurchase
+FROM TSQL2012.Sales.Orders o
+GROUP BY o.CustomerID;
+24. SELECT 
+    YEAR(OrderDate) AS OrderYear,
+    MONTH(OrderDate) AS OrderMonth,
+    SUM(TotalAmount) AS TotalSales,
+    COUNT(DISTINCT ProductID) AS UniqueProductsSold
+FROM Orders
+GROUP BY YEAR(OrderDate), MONTH(OrderDate)
+HAVING COUNT(DISTINCT ProductID) >= 2;
+25. SELECT 
+    YEAR(OrderDate) AS OrderYear,
+    MIN(Quantity) AS MinOrderQuantity,
+    MAX(Quantity) AS MaxOrderQuantity
+FROM Orders
+GROUP BY YEAR(OrderDate);
